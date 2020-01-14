5298c4d1abca8cbeb554453783791144d612e814
eridhobffry <eridhobffry@github.com>
2020-01-14 12:47:51 +0100

    sagas

---
+++ src/sagas/authenticationSaga.js
@@ -0,0 +1,32 @@
+import { put, call } from "redux-saga/effects";
+import {
+  // registerUserService,
+  loginUserService
+} from "../services/authServices";
+import {
+  loginRequestSuccess,
+  loginRequestError
+} from "../actions/authenticationActions";
+import * as types from "../general/constants";
+
+// export function* registerSaga(payload) {
+//   try {
+//     const response = yield call(registerUserService, payload);
+//     yield [
+//       put({ type: types.REGISTER_USER_SUCCESS, response })
+//     ];
+//   } catch(error) {
+//     yield put({ type: types.REGISTER_USER_ERROR, error });
+//   }
+// }
+
+export function* loginSaga(payload) {
+  try {
+    const response = yield call(loginUserService, payload);
+    yield put(loginRequestSuccess(response, payload.hash));
+    // yield [put({ type: types.LOGIN_USER_SUCCESS, response })];
+  } catch (error) {
+    yield put(loginRequestError(error.reasonKey));
+    // yield put({ type: types.LOGIN_USER_ERROR, error });
+  }
+}
---
+++ src/sagas/watchers.js
@@ -0,0 +1,12 @@
+import { takeLatest } from "redux-saga/effects";
+import {
+  // registerSaga,
+  loginSaga
+} from "./authenticationSaga";
+
+import * as types from "../general/constants";
+
+export default function* watchUserAuthentication() {
+  // yield takeLatest(types.REGISTER_USER, registerSaga);
+  yield takeLatest(types.LOGIN_USER, loginSaga);
+}
